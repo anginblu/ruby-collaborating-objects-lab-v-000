43d84c512144ffd404bb18cf1a83c3a3e565909a
Jacqueline Lee <anginblu@gmail.com>
2017-11-28 04:41:11 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,23 +21,23 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    find(name) ? find(name) : create(name)
-#    if @@all.any?{|i| i.name == name}
-#      new_artist = @@all.find{|i| i.name == name}
-#    else
-#      new_artist = Artist.new(name)
-#      new_artist.save
-#    end
-#    new_artist
+#    find(name) ? find(name) : create(name)
+    if @@all.any?{|i| i.name == name}
+      new_artist = @@all.find{|i| i.name == name}
+    else
+      new_artist = Artist.new(name)
+      new_artist.save
+    end
+    new_artist
   end
 
-  def self.find(name)
+#  def self.find(name)
     all.find{ |artist| artist.name == name }
   end
 
-  def self.create(name)
-    new(name).tap { |artist| artist.save }
-  end
+#  def self.create(name)
+#    new(name).tap { |artist| artist.save }
+#  end
 
   def print_songs
     self.songs.each do |i|
